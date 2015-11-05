6b6ee0251dfbece9e71a04a6c564165146360933
Brian Lee <brianlee8318@gmail.com>
2015-11-04 19:40:04 -0600

    MvvmApp Library
    ViewModel
    ViewModelActivity
    ViewModelFragment

---
+++ mvvmapp/src/main/java/com/hibrianlee/mvvmapp/fragment/ViewModelFragment.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2015 Brian Lee (@hiBrianLee)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hibrianlee.mvvmapp.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+
+import com.hibrianlee.mvvmapp.viewmodel.ViewModel;
+
+public abstract class ViewModelFragment extends Fragment {
+
+    private static final String EXTRA_VIEW_MODEL_STATE = "viewModelState";
+
+    private ViewModel viewModel;
+
+    @Nullable
+    protected abstract ViewModel createViewModel(@Nullable ViewModel.State savedViewModelState);
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ViewModel.State savedViewModelState = null;
+        if (savedInstanceState != null) {
+            savedViewModelState = savedInstanceState.getParcelable(EXTRA_VIEW_MODEL_STATE);
+        }
+        viewModel = createViewModel(savedViewModelState);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (viewModel != null) {
+            viewModel.onStart();
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (viewModel != null) {
+            outState.putParcelable(EXTRA_VIEW_MODEL_STATE, viewModel.getInstanceState());
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (viewModel != null) {
+            viewModel.onStop();
+        }
+    }
+}
+
---
+++ mvvmapp/src/main/java/com/hibrianlee/mvvmapp/viewmodel/ViewModel.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2015 Brian Lee (@hiBrianLee)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hibrianlee.mvvmapp.viewmodel;
+
+import android.databinding.BaseObservable;
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.support.annotation.CallSuper;
+import android.support.annotation.Nullable;
+
+public abstract class ViewModel extends BaseObservable {
+
+    protected ViewModel(@Nullable State savedInstanceState) {
+
+    }
+
+    @CallSuper
+    public void onStart() {
+
+    }
+
+    public State getInstanceState() {
+        return new State(this);
+    }
+
+    @CallSuper
+    public void onStop() {
+
+    }
+
+    public static class State implements Parcelable {
+
+        protected State(ViewModel viewModel) {
+
+        }
+
+        public State(Parcel in) {
+
+        }
+
+        @Override
+        public int describeContents() {
+            return 0;
+        }
+
+        @CallSuper
+        @Override
+        public void writeToParcel(Parcel dest, int flags) {
+
+        }
+
+        public static Creator<State> CREATOR = new Creator<State>() {
+            @Override
+            public State createFromParcel(Parcel source) {
+                return new State(source);
+            }
+
+            @Override
+            public State[] newArray(int size) {
+                return new State[size];
+            }
+        };
+    }
+}
+
